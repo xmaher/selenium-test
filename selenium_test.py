cf241d9091c5a11687a5a4d48a5c286ab3fa3342
xmaher <maher.ka@gmail.com>
2021-02-14 19:06:32 +0200

    add my first file

---
+++ selenium_test.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+
+# change driver location to match your system
+chrome_driver_exe = r"drivers\chromedriver.exe"
+url = r'https://www.google.com'
+textbox_full_xpath = "/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input"
+button_full_xpath = "/html/body/div[1]/div[3]/form/div[2]/div[1]/div[3]/center/input[1]"
+search_phrase = "i just googled using selenium, invoked by jenkins pipeline"
+
+chromedriver = webdriver.Chrome(executable_path=chrome_driver_exe)
+chromedriver.get(url)
+element = chromedriver.find_element_by_xpath(textbox_full_xpath)
+element.send_keys(search_phrase)
+chromedriver.find_element_by_xpath(button_full_xpath).click()
